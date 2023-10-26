74a78215b88cd8015d3f9cc2c8026e46fa65b64f
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-10-26 16:19:26 -0500

    version 12

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use("/api/Tarea", TareaRouter);
 
 
 // para correr el servidor
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log("Servidor ejecutandose en el puerto 4000");
